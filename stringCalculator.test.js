21025b807341dc9f58760c5504770431d4ee5ffa
Sumit Kumar <sumit.kumar@aroha.co.in>
2024-12-14 15:53:47 +0530

    test cases added

---
+++ stringCalculator.test.js
@@ -0,0 +1,33 @@
+const { add } = require('./stringCalculator');
+
+test('should return 0 for an empty string', () => {
+  expect(add("")).toBe(0);
+});
+
+test('should return the number itself for a single number', () => {
+  expect(add("1")).toBe(1);
+});
+
+test('should return the sum of two numbers', () => {
+  expect(add("1,2")).toBe(3);
+});
+
+test('should return the sum of multiple numbers', () => {
+  expect(add("1,2,3,4,5")).toBe(15);
+});
+
+test('should handle newlines as delimiters', () => {
+  expect(add("1\n2,3")).toBe(6);
+});
+
+test('should handle custom delimiters', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
+
+test('should throw an exception for negative numbers', () => {
+  expect(() => add("1,-2,3")).toThrow("negative numbers not allowed: -2");
+});
+
+test('should list all negative numbers in the exception', () => {
+  expect(() => add("1,-2,-3")).toThrow("negative numbers not allowed: -2,-3");
+});
