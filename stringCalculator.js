8351c6b37feb758823cc3002532fd387cc442cf2
Sumit Kumar <sumit.kumar@aroha.co.in>
2024-12-14 15:49:05 +0530

    Project setup and basic calculation

---
+++ stringCalculator.js
@@ -0,0 +1,17 @@
+function add(numbers) {
+    if (numbers === "") return 0;
+  
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n", 2);
+      delimiter = new RegExp(parts[0].substring(2));
+      numbers = parts[1];
+    }
+  
+    const numArray = numbers.split(delimiter).map((num) => parseInt(num, 10));
+  
+    return numArray.reduce((sum, num) => sum + num, 0);
+  }
+  
+  module.exports = { add };
+  
